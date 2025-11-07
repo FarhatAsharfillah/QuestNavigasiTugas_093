07f42b230822cfaf5dddccbf136eaa802d0449ad
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-11-07 19:30:11 +0700

    menambahkan horizontaldivider modifier padding dan width

--- app/src/main/java/com/example/questnavigasitugas_093/ui/theme/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_093/ui/theme/view/Formulir.kt
@@ -73,3 +73,9 @@ Scaffold (modifier = Modifier,
                 }
             }
         }
+        HorizontalDivider(modifier = Modifier
+            .padding(20.dp)
+            .width(250.dp),
+            thickness = 1.dp,
+            color = Color.Red
+        )
